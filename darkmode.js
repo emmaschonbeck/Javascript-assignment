56c2ce9853139bf24ed0dbeb82b65a820be8e648
Emma <emmaschonbeck7@gmail.com>
2024-10-18 13:10:49 +0200

    Darkmode is done, started withe contact page

---
+++ darkmode.js
@@ -0,0 +1,36 @@
+
+// Darkmode koden är tagen av filen som fanns i Omniway
+const darkmodeSwitch = document.querySelector('#darkmode-switch')
+const hasDarkmode = localStorage.getItem('darkmode')
+
+if(hasDarkmode == null) {
+  if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    enableDarkMode()
+  } else {
+    disableDarkMode()
+  }
+} else if(hasDarkmode === 'on') {
+  enableDarkMode()
+} else if(hasDarkmode === 'off') {
+  disableDarkMode()
+}
+
+
+darkmodeSwitch.addEventListener('change', () => {
+  if(darkmodeSwitch.checked) {
+    enableDarkMode()
+    localStorage.setItem('darkmode', 'on')
+  } else {
+    disableDarkMode()
+    localStorage.setItem('darkmode', 'off')
+  }
+})
+
+function enableDarkMode() {
+  darkmodeSwitch.checked = true
+  document.documentElement.classList.add('dark')
+}
+function disableDarkMode() {
+  darkmodeSwitch.checked = false
+  document.documentElement.classList.remove('dark')
+}
